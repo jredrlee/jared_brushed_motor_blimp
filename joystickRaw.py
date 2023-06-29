6a48377a610e30d239cc63aee5c576d413701b00
jredrlee <46541732+jredrlee@users.noreply.github.com>
2023-06-29 18:02:38 -0400

    udp feedback implemented for joystickRAW

--- joystickRaw.py
+++ joystickRaw.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 import time
 import socket
@@ -15,6 +16,11 @@ def udp_init():
         socket.AF_INET, # Internet
         socket.SOCK_DGRAM
     ) # UDP
+    # allows sock to obtain data without stopping to wait for it
+    sock.setblocking(1)
+
+    # sock binds to the interfaces and uses port 1333
+    sock.bind(('', 1333))
     return sock
 
 def joystick_init():
@@ -34,8 +40,35 @@ def init():
 def udp_send(sock, ip, port, message):
     sock.sendto(message, (UDP_IP, UDP_PORT))
 
+##################################################################
+#Code implemented from: https://www.youtube.com/watch?v=PQ27j8WAP8Q
 
+def check_data():
+    try:
+        # Data received
+        data, addr = sock.recvfrom(1024)
+        #print("received message: %s from %s" % (data,addr))
+
+        # return the decoded bytes as a string
+        return data.decode()
+    # If no data is received just return None
+    except socket.error:
+        return None
+
+def main():
+    # Main loop
+    while True:
+        # Check for UDP data
+        line = check_data()
+        # If there is data split it and print it to console
+        if line:
+            split_line = line.split('|')
+            print(split_line)
+
+#####################################################################
+        
 if __name__ == "__main__":
+
     sock, joystick = init()
     l = 0.2 # meters
     absz = 0
@@ -43,51 +76,63 @@ if __name__ == "__main__":
     b_state = 1
     tauz = 0
     fx = 0
-    while True:
-        time_start = time.time()
-        # Get the joystick readings
-        pygame.event.pump()
-        b = joystick.get_button(1)
-        if b == 1 and b_old == 0:
-            b_state = not b_state
-        b_old = b
-        if abs(joystick.get_axis(4) )> .1:
-            fx = -1*joystick.get_axis(4) # left handler: up-down, inverted
-        else:
-            fx = 0
-        taux = 0#joystick.get_axis(3) # left handler: left-right
-        fz = 0#-2*joystick.get_axis(1)  # right handler: up-down, inverted
-        if abs(joystick.get_axis(3)) > .2:
-            tauz = -0.6*joystick.get_axis(3) # right handler: left-right
-        else:
-            tauz = 0
-        fy = 0
-        tauy = 0
-        #absz = .5
-        if abs(joystick.get_axis(1)) > .15:
-            absz += -0.005*joystick.get_axis(1)
-        if  b_state == 1:
-            absz = .2
-        
 
-        
-        # print(fx, taux, fz, tauz)
+    try:
 
-        # f1x = (fx - tauz/l)/2
-        # f2x = (fx + tauz/l)/2
-        # f1z = (fz + taux/l)/2
-        # f2z = (fz - taux/l)/2
+        while True:
+            time_start = time.time()
+            # Get the joystick readings
+            pygame.event.pump()
+            b = joystick.get_button(1)
+            if b == 1 and b_old == 0:
+                b_state = not b_state
+            b_old = b
+            if abs(joystick.get_axis(4) )> .1:
+                fx = -1*joystick.get_axis(4) # left handler: up-down, inverted
+            else:
+                fx = 0
+            taux = 0#joystick.get_axis(3) # left handler: left-right
+            fz = 0#-2*joystick.get_axis(1)  # right handler: up-down, inverted
+            if abs(joystick.get_axis(3)) > .2:
+                tauz = -0.6*joystick.get_axis(3) # right handler: left-right
+            else:
+                tauz = 0
+            fy = 0
+            tauy = 0
+            #absz = .5
+            if abs(joystick.get_axis(1)) > .15:
+                absz += -0.005*joystick.get_axis(1)
+            if  b_state == 1:
+                absz = .2
+            
 
-        # f1 = math.sqrt(f1x**2 + f1z**2) *255/3
-        # f2 = math.sqrt(f2x**2 + f2z**2) *255/3
-        # t1 = math.atan2(f1z, f1x)*180/math.pi
-        # t2 = math.atan2(f2z, f2x)*180/math.pi
-        print(round(fx,2), round(fz,2), round(taux,2), round(tauz,2), round(absz,2))
-        # print()
-        
-        message = struct.pack('<ffffffff', fx, fy , fz, taux, tauy, tauz, absz, b_state) 
-        udp_send(sock, UDP_IP, UDP_PORT, message)
-        #print(message)
-        time.sleep(0.005) #0.005
-        #while(time.time() - time_start < 0.01):
-            #time.sleep(0.001) #0.005
+            
+            # print(fx, taux, fz, tauz)
+
+            # f1x = (fx - tauz/l)/2
+            # f2x = (fx + tauz/l)/2
+            # f1z = (fz + taux/l)/2
+            # f2z = (fz - taux/l)/2
+
+            # f1 = math.sqrt(f1x**2 + f1z**2) *255/3
+            # f2 = math.sqrt(f2x**2 + f2z**2) *255/3
+            # t1 = math.atan2(f1z, f1x)*180/math.pi
+            # t2 = math.atan2(f2z, f2x)*180/math.pi
+
+            #print(round(fx,2), round(fz,2), round(taux,2), round(tauz,2), round(absz,2))
+
+            # print()
+            
+            message = struct.pack('<ffffffff', fx, fy , fz, taux, tauy, tauz, absz, b_state) 
+            udp_send(sock, UDP_IP, UDP_PORT, message)
+            #print(message)
+
+            main()
+
+
+            time.sleep(0.005) #0.005
+            #while(time.time() - time_start < 0.01):
+                #time.sleep(0.001) #0.005
+    except KeyboardInterrupt:
+        print("The end")
+        sys.exit()
